f64c3646904ab926ba463c6bbbc743ad8a90acef
Diana Ilina <diana.1993.30@gmail.com>
2023-02-13 22:27:59 +0000

    Adding while circle

--- Bot.py
+++ Bot.py
@@ -1,19 +1,26 @@
 help = """
 add - добавить задачу в список;
 help - показать команды;
-show - показать список дел.
+show - показать список дел
+end - закончить выполнение программы.
 """
 
 tasks = []
 
-command = input("Введите команду: ")
-if command == "help":
-    print(help)
-elif command == "show":
-    print(tasks)
-elif command =="add":
-    task = input("Введите задачу: ")
-    tasks.append(task)
-    print("Задача добавлена")
-else:
-    print("Неизвестная команда")
+run = True
+
+while run:
+    command = input("Введите команду: ")
+    if command == "help":
+        print(help)
+    elif command == "show":
+        print(tasks)
+    elif command =="add":
+        task = input("Введите задачу: ")
+        tasks.append(task)
+        print("Задача добавлена")
+    elif command == "end":
+        break
+    else:
+        print("Неизвестная команда")
+print("Завершение работы")
