263c7a95a155380b25164c04f8e7606a36a43e01
Diana Ilina <diana.1993.30@gmail.com>
2023-03-12 15:15:49 +0000

    Adding save(tag) function

--- Telebot.py
+++ Telebot.py
@@ -9,22 +9,31 @@ token = '5645737999:AAFguhZtbvmcqosgDV4zO_-Bhu26mI3dG38'
 bot = telebot.TeleBot(token)
 
 HELP = """
-/add - добавить задачу в список;
+/add - добавить в список;
 /end - закончить выполнение программы;
 /help - вывести список доступных команд;
-/random - добавить случайную задачу на сегодня;
-/show - показать список дел.
+/show - показать список;
+/save - добавить новый хэштег и/или добавить новую запись в существующий хэштег.
 """
 
-with open("Help.json", "w")as f:
+with open("Help.json", "w") as f:
     json.dump(HELP, f, indent=4)
 
-with open("Help.json")as f:
+with open("Help.json") as f:
     HELP = json.load(f)
 
 tasks = {}
 
-random_tasks = ["sport", "grocery", "playing piano", "study"]
+saves = {}
+
+with open("Saves.json", "w") as f:
+    json.dump(saves, f, indent=4)
+
+with open("Saves.json") as f:
+    saves = json.load(f)
+
+
+# random_tasks = ["sport", "grocery", "playing piano", "study"]
 
 
 def add_todo(date, task):
@@ -33,6 +42,14 @@ def add_todo(date, task):
     else:
         tasks[date] = []
         tasks[date].append(task)
+
+
+def save_me(key, note):
+    if key in saves:
+        saves[key].append(note)
+    else:
+        saves[key] = []
+        saves[key].append(note)
 
 
 @bot.message_handler(commands=["help"])
@@ -50,13 +67,23 @@ def add(message):
     bot.send_message(message.chat.id, text)
 
 
-@bot.message_handler(commands=["random"])
-def random_add(message):
-    date = "cегодня"
-    task = random.choice(random_tasks)
-    add_todo(date, task)
-    text = "На сегодня появилась задача " + task + "!"
+@bot.message_handler(commands=["save"])
+def save(message):
+    command = message.text.split(maxsplit=2)
+    key = command[1].lower()
+    note = command[2]
+    save_me(key, note)
+    text = "Запись " + note + " добавлена в хэштег " + key
     bot.send_message(message.chat.id, text)
+
+
+# @bot.message_handler(commands=["random"])
+# def random_add(message):
+#     date = "cегодня"
+#     task = random.choice(random_tasks)
+#     add_todo(date, task)
+#     text = "На сегодня появилась задача " + task + "!"
+#     bot.send_message(message.chat.id, text)
 
 
 @bot.message_handler(commands=["show", "print"])
@@ -72,6 +99,19 @@ def show_commands(message):
         text = "День свободен"
     bot.send_message(message.chat.id, text)
 
+
+@bot.message_handler(commands=["show", "print"])
+def show_commands(message):
+    command = message.text.split(maxsplit=1)
+    key = command[1].lower()
+    text = ""
+    if key in saves:
+        text = key.upper() + "\n"
+        for task in tasks[date]:
+            text = text + "[] " + task + "\n"  # \n - перевод строки
+    else:
+        text = "День свободен"
+    bot.send_message(message.chat.id, text)
 
 # функция polling начинает отправку запросов в телеграм с заданным токеном и спрашивает, нет ли для него сообщений.
 # Если сообщение есть, то вызывается обработка. Long polling - процесс постоянного обращения к серверам
